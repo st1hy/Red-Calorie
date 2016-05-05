d0e6696e9ea3b32f9f8c6b00be7fecfd189048e0
st1hy <st1hy@outlook.com>
2016-05-05 13:35:44 +0200

    Removed unused test utilities.

--- app/src/test/java/rx/plugins/TestRxPlugins.java
+++ app/src/test/java/rx/plugins/TestRxPlugins.java
@@ -9,7 +9,6 @@ import rx.android.plugins.RxAndroidSchedulersHook;
 public class TestRxPlugins {
 
     public static void registerImmediateMainThreadHook() {
-
         RxAndroidPlugins.getInstance().reset();
         RxAndroidPlugins.getInstance().registerSchedulersHook(new RxAndroidSchedulersHook() {
             @Override
@@ -20,7 +19,6 @@ public class TestRxPlugins {
     }
 
     public static void reset() {
-//        RxJavaPlugins.getInstance().reset();
         RxAndroidPlugins.getInstance().reset();
         Schedulers.reset();
     }
@@ -51,38 +49,4 @@ public class TestRxPlugins {
             }
         });
     }
-//
-//    public static void registerImmediateHookAll() {
-////        RxJavaPlugins.getInstance().reset();
-////        RxJavaPlugins.getInstance().registerSchedulersHook(new RxJavaSchedulersHook() {
-////            @Override
-////            public Scheduler getIOScheduler() {
-////                return rx.schedulers.Schedulers.immediate();
-////            }
-////
-////        });
-//        RxAndroidPlugins.getInstance().reset();
-//        RxAndroidPlugins.getInstance().registerSchedulersHook(new RxAndroidSchedulersHook() {
-//            @Override
-//            public Scheduler getMainThreadScheduler() {
-//                return rx.schedulers.Schedulers.immediate();
-//            }
-//        });
-//        Schedulers.registerHook(new Schedulers.Hook() {
-//            @Override
-//            public Scheduler io() {
-//                return rx.schedulers.Schedulers.immediate();
-//            }
-//
-//            @Override
-//            public Scheduler computation() {
-//                return rx.schedulers.Schedulers.computation();
-//            }
-//
-//            @Override
-//            public Scheduler immediate() {
-//                return rx.schedulers.Schedulers.immediate();
-//            }
-//        });
-//    }
 }
