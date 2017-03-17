b2af82cfb96b995b6e902f1aa25fa31e2f70f624
st1hy <st1hy@outlook.com>
2017-03-17 19:03:16 +0100

    Fixed memory leak by removing hiddden reference to view in ResultReceiver

--- app/src/main/java/com/github/st1hy/countthemcalories/core/rx/RxInputMethodManager.java
+++ app/src/main/java/com/github/st1hy/countthemcalories/core/rx/RxInputMethodManager.java
@@ -19,30 +19,39 @@ public class RxInputMethodManager {
     @CheckResult
     @NonNull
     public static Observable<Integer> showSoftInput(InputMethodManager manager, View view, int flags) {
-        return Observable.create(new Observable.OnSubscribe<Integer>() {
-            @Override
-            public void call(Subscriber<? super Integer> subscriber) {
-                AtomicReference<Action1<Integer>> ref = new AtomicReference<>((t) -> {
-                    subscriber.onNext(t);
-                    subscriber.onCompleted();
-                });
-                manager.showSoftInput(view, flags, new ResultReceiver(null) {
-                    @Override
-                    protected void onReceiveResult(int resultCode, Bundle resultData) {
-                        super.onReceiveResult(resultCode, resultData);
-                        Action1<Integer> action = ref.get();
-                        if (!subscriber.isUnsubscribed() && action != null) {
-                            action.call(resultCode);
-                        }
-                    }
-                });
-                subscriber.add(new MainThreadSubscription() {
-                    @Override
-                    protected void onUnsubscribe() {
-                        ref.set(null);
-                    }
-                });
-            }
+        return Observable.create(subscriber -> {
+            AtomicReference<Action1<Integer>> ref = new AtomicReference<>((t) -> {
+                subscriber.onNext(t);
+                subscriber.onCompleted();
+            });
+            manager.showSoftInput(view, flags, new MResultReceiver(ref, subscriber));
+            subscriber.add(new MainThreadSubscription() {
+                @Override
+                protected void onUnsubscribe() {
+                    ref.set(null);
+                }
+            });
         });
+    }
+
+    private static class MResultReceiver extends ResultReceiver {
+        private final AtomicReference<Action1<Integer>> actionRef;
+        private final Subscriber<? super Integer> subscriber;
+
+        public MResultReceiver(AtomicReference<Action1<Integer>> actionRef,
+                               Subscriber<? super Integer> subscriber) {
+            super(null);
+            this.actionRef = actionRef;
+            this.subscriber = subscriber;
+        }
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            super.onReceiveResult(resultCode, resultData);
+            Action1<Integer> action = actionRef.get();
+            if (!subscriber.isUnsubscribed() && action != null) {
+                action.call(resultCode);
+            }
+        }
     }
 }
